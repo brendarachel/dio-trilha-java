7529df0a2b7fc55391cb586b6f04b11885f6fbf4
Brenda Rachel <114029722+brendarachel@users.noreply.github.com>
2022-10-13 22:01:48 -0300

    Add files via upload

---
+++ dio/metodo/Metodo.java
@@ -0,0 +1,20 @@
+package dio.metodo;
+
+public class Metodo {
+    
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Brenda";
+        String segundoNome = "Rachel";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    // o método deve conter o nome (geralmente no infinitivo, mas neste caso não) e os parâmetros em parênteses
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+}
+
